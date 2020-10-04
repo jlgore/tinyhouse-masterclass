3495fbbf782069c87eaa8668930df77ab8ff70c6
jared <jared@madstone.io>
2020-10-04 11:15:53 -0500

    modules 1-4

---
+++ server/src/database/index.ts
@@ -0,0 +1,21 @@
+require('dotenv').config()
+import { MongoClient } from "mongodb"
+import { Database } from '../lib/types'
+
+
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.jl7nm.mongodb.net/main?retryWrites=true&w=majority`
+
+export const connectDatabase = async (): Promise<Database> => {
+    const client = await MongoClient.connect(url, {
+         useNewUrlParser: true, 
+         useUnifiedTopology: true
+        });
+
+
+    const db = client.db('main');
+
+    return {
+        listings: db.collection('test_listings')
+    }
+};
+
